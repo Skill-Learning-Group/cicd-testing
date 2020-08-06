e25abe1339600ecb06c03ff7e6fefb039fd613b8
Jeffrey Ying <jeffrey.ying@gmail.com>
2020-08-06 23:44:01 +0800

    unit test for article

--- server/controllers/article.controller.js
+++ server/controllers/article.controller.js
@@ -29,7 +29,7 @@ class ArticleController {
 
   static async get(req, res, next) {
     try {
-      const data = await Article.findOne({ id: req.params.id });
+      const data = await Article.findOne({ _id: req.params.id });
       return data ? res.status(200).json(APIsuccess(200, data)) : res.status(404).json(APIerror(404, { message: `Article not found` }));
     } catch (error) {
       next(error);
@@ -48,7 +48,7 @@ class ArticleController {
   static async remove(req, res, next) {
     try {
       const data = await Article.findByIdAndDelete(req.params.id);
-      return data ? res.status(201).send() : res.status(404).json(APIerror(404, { message: `Article not found` }));
+      return data ? res.status(200).send() : res.status(404).json(APIerror(404, { message: `Article not found` }));
     } catch (error) {
       next(error);
     }
